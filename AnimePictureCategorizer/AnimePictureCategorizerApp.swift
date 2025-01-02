611e8d5383adbe00afe44bd5d202247feca2446c
shinoxzu <shinoxzu@gmail.com>
2025-01-03 00:24:05 +0300

    Initial Commit

---
+++ AnimePictureCategorizer/AnimePictureCategorizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimePictureCategorizerApp.swift
+//  AnimePictureCategorizer
+//
+//  Created by Антон Романов on 03.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimePictureCategorizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimePictureCategorizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimePictureCategorizer
+//
+//  Created by Антон Романов on 03.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
